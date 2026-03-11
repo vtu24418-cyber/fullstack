05e464450600d23b97ebfe858be2a015a19dc076
vtu24418-cyber <vtu24418@veltech.edu.in>
2026-03-11 10:03:43 +0530

    Delete SpringCarDemo directory

--- SpringCarDemo/src/main/java/com/example/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-@ComponentScan(basePackages = "com.example")
-public class AppConfig {
-}
